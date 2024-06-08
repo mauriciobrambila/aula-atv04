54919a723608430e18ae1988fa750eb37dbe1a6a
mauriciobrambila <mauriciobrambila2015@gmail.com>
2024-06-07 21:07:15 -0300

    commit testando docker

--- app.js
+++ app.js
@@ -1,6 +1,39 @@
 const express = require("express");
+const mysql = require('mysql2');
 const os = require("os");
 const app = express();
+
+const dbConfig = {
+    host: process.env.DB_HOST || 'localhost',
+    user: process.env.DB_USER || 'root',
+    password: process.env.DB_PASSWORD || '',
+    database: process.env.DB_NAME || 'aula04'
+}; 
+
+const connection = mysql.createConnection(dbConfig);
+connection.connect(err => {
+    if (err) {
+        console.error('Erro ao conectar ao banco de dados:', err);
+        return;
+    }
+    console.log('Conectado com sucesso ao banco de dados MySQL');
+});
+
+app.get('/consulta-dados', (request, response) => {
+    const query = 'SELECT * FROM contatos';
+
+    connection.query(query, (err, results) => {
+        if (err) {
+            console.error('Erro ao executar a consulta:', err);
+            return response.status(500).json({
+                error: 'Erro ao buscar dados do banco de dados'
+        });
+    }
+    return response.status(200).json({
+        data: results
+        });
+    });
+});
 
 
 app.get("/", (request, response) => {
---
+++ init.sql
@@ -0,0 +1,17 @@
+CREATE DATABASE IF NOT EXISTS aula04;
+USE aula04;
+
+CREATE TABLE contatos (
+        id INT AUTO_INCREMENT PRIMARY KEY,
+        nome VARCHAR(100) NOT NULL,
+        cidade VARCHAR(100) NOT NULL,
+        telefone VARCHAR(20) NOT NULL
+);
+
+
+INSERT INTO contatos (nome, cidade, telefone) VALUES
+('Maria Silva', 'São Paulo', '(11) 98765-4321'),
+('João Souza', 'Rio de Janeiro', '(21) 91234-5678'),
+('Ana Oliveira', 'Belo Horizonte', '(31) 99876-5432'),
+('Carlos Pereira', 'Porto Alegre', '(51) 98765-1234');
+
